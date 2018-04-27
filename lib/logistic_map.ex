99b820d0af589f995d271e4703e06a8e84061d73
Susumu Yamazaki <zacky1972@gmail.com>
2018-04-28 04:59:52 +0900

    implement logistic map benchmark

--- lib/logistic_map.ex
+++ lib/logistic_map.ex
@@ -4,15 +4,57 @@ defmodule LogisticMap do
   """
 
   @doc """
-  Hello world.
+  calc logistic map.
 
   ## Examples
 
-      iex> LogisticMap.hello
-      :world
+      iex> LogisticMap.calc(1, 61, 22)
+      22
 
   """
-  def hello do
-    :world
+  def calc(l, p, myu) do
+    rem(myu * l * (l * 1), p) 
+  end
+
+  @doc """
+  loop logistic map
+
+  ## Examples
+
+      iex> LogisticMap.loopCalc(10, 1, 61, 22)
+      56
+
+  """
+  def loopCalc(num, l, p, myu) do
+    if num <= 0 do
+      calc(l, p, myu)
+    else
+      loopCalc(num - 1, calc(l, p, myu), p, myu)
+    end
+  end
+
+  @doc """
+  Flow.map calc logictic map
+
+  ## Examples
+
+      iex> 1..3 |> LogisticMap.mapCalc(10, 61, 22)
+      [56, 1, 13]
+
+  """
+  def mapCalc(list, num, p, myu, stages) do
+    list
+    |> Flow.from_enumerable(stages: stages)
+    |> Flow.map(& loopCalc(num, &1, p, myu))
+    |> Enum.to_list
+  end
+
+  @doc """
+  Benchmark
+  """
+  def benchmark(stages) do
+    :timer.tc(fn -> LogisticMap.mapCalc(1..6_700_416, 10, 6_700_417, 22, stages) end)
+    |> elem(0)
+    |> Kernel./(1000000)
   end
 end
