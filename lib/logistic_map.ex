3ff0697e25a49ac0f6102b6bd92fa4aa1f829557
Susumu Yamazaki <zacky1972@gmail.com>
2018-04-28 21:05:09 +0900

    add benchmark 2 & 3

--- lib/logistic_map.ex
+++ lib/logistic_map.ex
@@ -50,6 +50,59 @@ defmodule LogisticMap do
   end
 
   @doc """
+  Flow.map calc logictic map
+
+  ## Examples
+
+      iex> 1..3 |> LogisticMap.mapCalc2(61, 22, 1)
+      [28, 25, 37]
+
+  """
+  def mapCalc2(list, p, myu, stages) do
+    list
+    |> Flow.from_enumerable(stages: stages)
+    |> Flow.map(& calc(&1, p, myu))
+    |> Flow.map(& calc(&1, p, myu))
+    |> Flow.map(& calc(&1, p, myu))
+    |> Flow.map(& calc(&1, p, myu))
+    |> Flow.map(& calc(&1, p, myu))
+    |> Flow.map(& calc(&1, p, myu))
+    |> Flow.map(& calc(&1, p, myu))
+    |> Flow.map(& calc(&1, p, myu))
+    |> Flow.map(& calc(&1, p, myu))
+    |> Flow.map(& calc(&1, p, myu))
+    |> Enum.to_list
+  end
+
+  @doc """
+  Flow.map calc logictic map
+
+  ## Examples
+
+      iex> 1..3 |> LogisticMap.mapCalc3(61, 22, 1)
+      [28, 25, 37]
+
+  """
+  def mapCalc3(list, p, myu, stages) do
+    list
+    |> Flow.from_enumerable(stages: stages)
+    |> Flow.map(& (&1
+      |> calc(p, myu)
+      |> calc(p, myu)
+      |> calc(p, myu)
+      |> calc(p, myu)
+      |> calc(p, myu)
+      |> calc(p, myu)
+      |> calc(p, myu)
+      |> calc(p, myu)
+      |> calc(p, myu)
+      |> calc(p, myu)
+      ))
+    |> Enum.to_list
+  end
+
+
+  @doc """
   Benchmark
   """
   def benchmark(stages) do
@@ -62,11 +115,55 @@ defmodule LogisticMap do
   end
 
   @doc """
+  Benchmark
+  """
+  def benchmark2(stages) do
+    IO.puts "stages: #{stages}"
+    IO.puts (
+      :timer.tc(fn -> LogisticMap.mapCalc2(1..0x2000000, 6_700_417, 22, stages) end)
+      |> elem(0)
+      |> Kernel./(1000000)
+    )
+  end
+
+
+  @doc """
+  Benchmark
+  """
+  def benchmark3(stages) do
+    IO.puts "stages: #{stages}"
+    IO.puts (
+      :timer.tc(fn -> LogisticMap.mapCalc3(1..0x2000000, 6_700_417, 22, stages) end)
+      |> elem(0)
+      |> Kernel./(1000000)
+    )
+  end
+
+  @doc """
   Benchmarks
   """
   def benchmarks() do
     [1, 2, 4, 8, 16, 32, 64, 128]
     |> Enum.map(& benchmark(&1))
+    |> Enum.to_list
+  end
+
+  @doc """
+  Benchmarks
+  """
+  def benchmarks2() do
+    [1, 2, 4, 8, 16, 32, 64, 128]
+    |> Enum.map(& benchmark2(&1))
+    |> Enum.to_list
+  end
+
+
+  @doc """
+  Benchmarks
+  """
+  def benchmarks3() do
+    [1, 2, 4, 8, 16, 32, 64, 128]
+    |> Enum.map(& benchmark3(&1))
     |> Enum.to_list
   end
 end
