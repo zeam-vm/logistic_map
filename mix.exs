28bfb08c510a5067e8b4e78a077aa7bc2b7bc70a
Susumu Yamazaki <zacky1972@gmail.com>
2018-04-29 09:34:44 +0900

    add ex_doc to deps

--- mix.exs
+++ mix.exs
@@ -27,7 +27,8 @@ defmodule LogisticMap.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:flow, "~> 0.12"}
+      {:flow, "~> 0.12"},
+      {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
 end
