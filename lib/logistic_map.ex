78cb647b31d3c3935e8c236c2b82746e05001042
Susumu Yamazaki <zacky1972@gmail.com>
2018-05-18 13:16:02 +0900

    add benchmarks4

--- lib/logistic_map.ex
+++ lib/logistic_map.ex
@@ -34,6 +34,23 @@ defmodule LogisticMap do
   end
 
   @doc """
+  loop logistic map
+
+  ## Examples
+
+      iex> LogisticMap.loopCalc2(10, 1, 61, 22)
+      28
+
+  """
+  def loopCalc2(num, x, p, mu) when num <= 0 do x end
+  def loopCalc2(num, x, p, mu) do
+    new_num = num - 1
+    new_calc = calc( x, p, mu )
+    loopCalc2( new_num, new_calc, p, mu )
+  end
+
+
+  @doc """
   Flow.map calc logictic map
 
   ## Examples
@@ -103,6 +120,22 @@ defmodule LogisticMap do
 
 
   @doc """
+  Flow.map calc logictic map
+
+  ## Examples
+
+      iex> 1..3 |> LogisticMap.mapCalc4(10, 61, 22, 1)
+      [28, 25, 37]
+
+  """
+  def mapCalc4(list, num, p, mu, stages) do
+    list
+    |> Flow.from_enumerable(stages: stages)
+    |> Flow.map(& loopCalc2(num, &1, p, mu))
+    |> Enum.to_list
+  end
+
+  @doc """
   Benchmark
   """
   def benchmark(stages) do
@@ -139,6 +172,18 @@ defmodule LogisticMap do
     )
   end
 
+
+  @doc """
+  Benchmark
+  """
+  def benchmark4(stages) do
+    IO.puts "stages: #{stages}"
+    IO.puts (
+      :timer.tc(fn -> mapCalc4(1..0x2000000, 10, 6_700_417, 22, stages) end)
+      |> elem(0)
+      |> Kernel./(1000000)
+    )
+  end
   @doc """
   Benchmarks
   """
@@ -164,6 +209,15 @@ defmodule LogisticMap do
   def benchmarks3() do
     [1, 2, 4, 8, 16, 32, 64, 128]
     |> Enum.map(& benchmark3(&1))
+    |> Enum.to_list
+  end
+
+  @doc """
+  Benchmarks
+  """
+  def benchmarks4() do
+    [1, 2, 4, 8, 16, 32, 64, 128]
+    |> Enum.map(& benchmark4(&1))
     |> Enum.to_list
   end
 end
