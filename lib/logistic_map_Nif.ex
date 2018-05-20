d1dff473703c4a669364849d484c31e593dd9465
zacky1972 <zacky1972@gmail.com>
2018-05-20 16:45:17 +0900

    create dummy function sum_list

--- lib/logistic_map_Nif.ex
+++ lib/logistic_map_Nif.ex
@@ -3,4 +3,6 @@ defmodule LogisticMapNif do
 
     # When your NIF is loaded, it will override this function.
     def calc(_x, _p, _mu), do: :erlang.nif_error(:nif_not_loaded)
+
+    def sum_list(_x), do: :erlang.nif_error(:nif_not_loaded)
 end
--- native/logistic_map/src/lib.rs
+++ native/logistic_map/src/lib.rs
@@ -2,7 +2,8 @@
 // #[macro_use] extern crate rustler_codegen;
 #[macro_use] extern crate lazy_static;
 
-use rustler::{NifEnv, NifTerm, NifResult, NifEncoder};
+use rustler::{NifEnv, NifTerm, NifResult, NifEncoder, NifError};
+use rustler::types::list::NifListIterator;
 
 mod atoms {
     rustler_atoms! {
@@ -15,7 +16,8 @@ mod atoms {
 
 rustler_export_nifs! {
     "Elixir.LogisticMapNif",
-    [("calc", 3, calc)],
+    [("calc", 3, calc),
+     ("sum_list", 1, sum_list)],
     None
 }
 
@@ -25,4 +27,17 @@ fn calc<'a>(env: NifEnv<'a>, args: &[NifTerm<'a>]) -> NifResult<NifTerm<'a>> {
     let mu: i64 = try!(args[2].decode());
 
     Ok((atoms::ok(), mu * x * (x + 1) % p).encode(env))
+}
+
+fn sum_list<'a>(env: NifEnv<'a>, args: &[NifTerm<'a>]) -> NifResult<NifTerm<'a>> {
+    let iter: NifListIterator = try!(args[0].decode());
+
+    let res: Result<Vec<i64>, NifError> = iter
+        .map(|x| x.decode::<i64>())
+        .collect();
+
+    match res {
+        Ok(result) => Ok(result.iter().fold(0, |acc, &x| acc + x).encode(env)),
+        Err(err) => Err(err),
+    }
 }
