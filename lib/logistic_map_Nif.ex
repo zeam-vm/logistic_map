0a0bf8d3c23ab0bb67c7fd8f47d2da4caca9db43
Susumu Yamazaki <zacky1972@gmail.com>
2018-05-25 08:37:50 +0900

    change indent of logistic_map_Nif.ex

--- lib/logistic_map_Nif.ex
+++ lib/logistic_map_Nif.ex
@@ -1,14 +1,14 @@
 defmodule LogisticMapNif do
-    use Rustler, otp_app: :logistic_map, crate: :logistic_map
+  use Rustler, otp_app: :logistic_map, crate: :logistic_map
 
-    # When your NIF is loaded, it will override this function.
-    def calc(_x, _p, _mu), do: :erlang.nif_error(:nif_not_loaded)
+  # When your NIF is loaded, it will override this function.
+  def calc(_x, _p, _mu), do: :erlang.nif_error(:nif_not_loaded)
 
-    def map_calc_list(_list, _num, _p, _mu), do: :erlang.nif_error(:nif_not_loaded)
+  def map_calc_list(_list, _num, _p, _mu), do: :erlang.nif_error(:nif_not_loaded)
 
-    def to_binary(_list), do: 
-    :erlang.nif_error(:nif_not_loaded)
+  def to_binary(_list), do: 
+  :erlang.nif_error(:nif_not_loaded)
 
-    def map_calc_binary(_binary, _num, _p, _mu), do:
-    :erlang.nif_error(:nif_not_loaded)
+  def map_calc_binary(_binary, _num, _p, _mu), do:
+  :erlang.nif_error(:nif_not_loaded)
 end
