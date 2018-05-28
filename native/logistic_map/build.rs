05ac4b264c5571d801c8a9491029a9e260180f2f
Susumu Yamazaki <zacky1972@gmail.com>
2018-05-28 19:50:21 +0900

    add #include <erl_nif.h>

--- native/logistic_map/build.rs
+++ native/logistic_map/build.rs
@@ -4,5 +4,6 @@ fn main() {
     gcc::Build::new()
                 .file("src/c/logimap.c")
                 .include("src")
+                .include("/usr/local/lib/erlang/erts-9.2/include/")
                 .compile("liblogimap.a");
 }
--- native/logistic_map/src/c/logimap.c
+++ native/logistic_map/src/c/logimap.c
@@ -1,3 +1,5 @@
+#include <erl_nif.h>
+
 long add_c(long x, long y) {
 	return x + y;
 }
